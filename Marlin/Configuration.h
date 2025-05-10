f4fb8a2d22551dff4da910581445e8f7c8451cb6
chrisj2000 <42233475+chrisj2000@users.noreply.github.com>
2025-05-10 14:09:21 +0800

    auth

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -61,7 +61,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(none, default config)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Chris Jones, Ender 3 V2 Neo - MKS robin E3D 1.1)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
